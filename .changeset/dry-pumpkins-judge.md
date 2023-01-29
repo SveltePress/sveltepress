d443bfa1540ae69bb64548002634301c23ee828b
Blackman99 <1197160272@qq.com>
2023-01-29 14:39:42 +0800

    feat: action button style optimize

---
+++ .changeset/dry-pumpkins-judge.md
@@ -0,0 +1,5 @@
+---
+"@svelte-press/theme-default": patch
+---
+
+feat: action button style optimize
--- packages/theme-default/src/components/ActionButton.svelte
+++ packages/theme-default/src/components/ActionButton.svelte
@@ -24,12 +24,12 @@
 <style>
   .svp-action {
     --at-apply: inline-flex items-center h-12 leading-12 
-      rounded-6 px-6 bg-white dark:bg-zinc-9 
+      rounded-6 px-6 bg-white dark:bg-zinc-8
       transition-300 transition-shadow
       hover:shadow dark:hover:shadow-gray-6;
   }
   .svp-action--primary {
-    --at-apply: svp-gradient-bg;
+    --at-apply: svp-gradient-bg text-white dark:text-warm-gray-8;
   }
   .external-icon {
     --at-apply: text-6 ml-2 flex items-center;
