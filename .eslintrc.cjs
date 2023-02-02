2120978a15903a473e429ad38477cf46b8e78336
Blackman99 <1197160272@qq.com>
2023-02-02 12:38:38 +0800

    lint: update svelte lint config to the latest version and remove local config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,3 @@
 module.exports = {
   extends: ['@casual-ui/eslint-config-svelte'],
-  rules: {
-    'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
-  },
 }
