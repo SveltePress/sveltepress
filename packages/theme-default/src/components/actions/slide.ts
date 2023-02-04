4dc02361f2fd0a201c6d5a18e6737b0948ac89a0
Dongsheng <1197160272@qq.com>
2023-02-04 18:27:02 +0800

    feat: slide action should set default max height

--- packages/theme-default/src/components/actions/slide.ts
+++ packages/theme-default/src/components/actions/slide.ts
@@ -1,13 +1,12 @@
 import type { Action } from 'svelte/action'
 
 const slide: Action = (node, show) => {
-  const initialHeight = node.offsetHeight
+  const initialHeight = `${node.offsetHeight}px`
   node.style.transition = 'max-height 300ms ease-in-out'
-  if (!show) node.style.maxHeight = '0'
-
+  node.style.maxHeight = show ? initialHeight : '0'
   return {
     update(show) {
-      node.style.maxHeight = show ? `${initialHeight}px` : '0'
+      node.style.maxHeight = show ? initialHeight : '0'
     },
   }
 }
