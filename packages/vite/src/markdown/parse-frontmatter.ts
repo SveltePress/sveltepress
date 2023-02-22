6fa01b7cbda8c347eac91169e96fde4f4b28b288
Blackman99 <1197160272@qq.com>
2023-02-22 12:00:47 +0800

    fix: development package entry

--- packages/vite/src/markdown/parse-frontmatter.ts
+++
@@ -1,9 +0,0 @@
-import type { Plugin } from 'unified'
-
-const parseFrontmatter: Plugin<any[], any> = () => {
-  return (tree, vFile) => {
-    console.log(vFile)
-  }
-}
-
-export default parseFrontmatter
