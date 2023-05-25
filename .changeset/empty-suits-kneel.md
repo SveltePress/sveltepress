e416732fff073628a3ccd0c8911a7a4fa7a237f4
Blackman99 <1197160272@qq.com>
2023-05-25 10:20:05 +0800

    fix: add max height for page toc

---
+++ .changeset/empty-suits-kneel.md
@@ -0,0 +1,6 @@
+---
+'@sveltepress/theme-default': patch
+'@sveltepress/create': patch
+---
+
+fix: add max height for page toc
--- packages/theme-default/src/components/Toc.svelte
+++ packages/theme-default/src/components/Toc.svelte
@@ -92,7 +92,7 @@
   }
 
   .anchors {
-    --at-apply: 'relative z-3 sm:w-[15vw]';
+    --at-apply: 'relative z-3 sm:w-[15vw] max-h-[70vh] overflow-y-auto overflow-x-hidden';
   }
   .anchors::after {
     --at-apply: 'absolute left-[1px] top-0 bottom-0 w-[1px] bg-light-7 dark:bg-gray-8 display-none sm:display-block';
