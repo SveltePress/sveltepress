a06f05552a3c37c6a879c747ba6277ab83f29d64
Blackman99 <1197160272@qq.com>
2023-06-28 11:32:42 +0800

    docs: disable kit auto sw register

--- packages/docs-site/svelte.config.js
+++ packages/docs-site/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
 import { SERVICE_WORKER_PATH } from '@sveltepress/theme-default'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +7,15 @@ const config = {
   extensions: ['.svelte', '.md'],
   preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'dist',
+      fallback: '404.html',
+    }),
     files: {
       serviceWorker: SERVICE_WORKER_PATH,
+    },
+    serviceWorker: {
+      register: false,
     },
   },
 }
