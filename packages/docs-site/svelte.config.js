b33a40940c9dd2a1ff20de877e9f1f2e22805cdc
Dongsheng Zhao <1197160272@qq.com>
2023-12-23 12:03:25 +0800

    fix: vitePreprocss import

--- packages/docs-site/svelte.config.js
+++ packages/docs-site/svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static'
 import { SERVICE_WORKER_PATH } from '@sveltepress/theme-default'
 
