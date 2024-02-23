111652a259e9a2fab7523b93428afda656a3b109
Dongsheng Zhao <1197160272@qq.com>
2024-02-23 19:36:51 +0800

    fix: external link condition #233

---
+++ .changeset/khaki-olives-reflect.md
@@ -0,0 +1,8 @@
+---
+"@sveltepress/theme-default": patch
+"@sveltepress/twoslash": patch
+"@sveltepress/create": patch
+"@sveltepress/vite": patch
+---
+
+fix: link external condition #233
--- packages/docs-site-zh/svelte.config.js
+++ packages/docs-site-zh/svelte.config.js
@@ -2,12 +2,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static'
 import { SERVICE_WORKER_PATH } from '@sveltepress/theme-default'
 
-const excludePaths = [
-  '/guide/..mailto:contact@example.com',
-  '/reference/.../',
-  '/.https://github.com/Blackman99/sveltepress',
-]
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md'],
@@ -24,11 +18,6 @@ const config = {
       register: false,
     },
     prerender: {
-      handleHttpError: ({ path, message }) => {
-        if (excludePaths.includes(path))
-          return
-        throw new Error(message)
-      },
       handleMissingId: 'ignore',
     },
   },
