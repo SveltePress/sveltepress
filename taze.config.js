3be117c342d59b409377e3e93caa15a31e802afe
Dongsheng Zhao <1197160272@qq.com>
2024-02-26 11:24:22 +0800

    chore: update deps

--- taze.config.js
+++ taze.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'taze'
 
 export default defineConfig({
   exclude: [
-    'vite',
-    '@sveltejs/kit',
+    '@iconify/utils',
+    'unocss',
   ],
   recursive: true,
   force: true,
   write: true,
   install: true,
-  packageMode: {
-  },
   depFields: {
     overrides: false,
   },
