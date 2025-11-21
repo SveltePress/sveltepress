a6cd3e55aa929fd7bf2d98246d13057d47556f48
Dongsheng Zhao <zhaodongsheng@sj1.cn>
2025-11-21 19:44:22 +0800

    chore: add auto check deps workflow

---
+++ check.mjs
@@ -0,0 +1,41 @@
+import { cwd } from 'node:process'
+import { $ } from 'zx'
+
+$.cwd = cwd()
+
+async function createChangeset() {
+  const changesetRes = $`npx changeset`
+  changesetRes.stdin.write('\b')
+  changesetRes.stdin.write('\n')
+  changesetRes.stdin.write('\n')
+  changesetRes.stdin.write('\n')
+  const listener = () => {
+    changesetRes.stdin.write('chore: update deps\n')
+  }
+  changesetRes.stdout.on('data', listener)
+  await changesetRes
+}
+
+async function reinstallPackageAndCreateGitCommit() {
+  await $`pnpm i`
+  await $`git config user.name "Dongsheng Zhao"`
+  await $`git config user.email "1197160272@qq.com"`
+  await $`git add -A`
+  await $`git commit -a -m "chore: update deps"`
+  await $`git push`
+}
+
+async function main() {
+  const gitStatusLines = (await $`git status`).toString().split('\n')
+
+  if (
+    gitStatusLines.some(line => line.startsWith('Untracked files:')
+      || line.startsWith('Changes not staged for commit:'
+        || line.startsWith('Changes to be committed:')))
+  ) {
+    await createChangeset()
+    await reinstallPackageAndCreateGitCommit()
+  }
+}
+
+main()
