18b493924def382cbddc8be50f58bef0407671e2
Scott Wu <sw@scottwu.ca>
2025-12-19 22:17:36 +0800

    test code

--- packages/docs-site/vite.config.ts
+++ packages/docs-site/vite.config.ts
@@ -41,7 +41,7 @@ const config = defineConfig({
         },
         highlighter: {
           twoslash: true,
-          languages: ['svelte', 'sh', 'js', 'html', 'ts', 'md', 'css', 'scss', 'json'],
+          languages: ['svelte', 'sh', 'js', 'html', 'ts', 'md', 'css', 'scss', 'json', 'go'],
         },
       }),
       siteConfig: {
