fab22e8ecaaba0dff26c05435a9ef5bc17ba4fb9
Dongsheng Zhao <1197160272@qq.com>
2025-12-25 13:28:59 +0800

    test: add inline code in admonition test case

--- packages/theme-default/__tests__/admonitions.test.ts
+++ packages/theme-default/__tests__/admonitions.test.ts
@@ -59,4 +59,20 @@ const obj = {}
       </div></div></div>"
     `)
   })
+
+  it('with inline code', async () => {
+    const source = `
+:::tip[Tip]
+Here is some \`inline code\`
+:::
+`
+    const { code } = await mdToSvelte({
+      filename: 'with-code-block.md',
+      mdContent: source,
+      highlighter,
+      remarkPlugins: [admonitions],
+    })
+
+    expect(code).toMatchInlineSnapshot(`"<div class="admonition admonition-tip"><div class="admonition-heading"><span class="admonition-icon"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M14.5 9.5C14.5 6.47 12.03 4 9 4S3.5 6.47 3.5 9.5c0 2.47 1.49 3.89 2.35 4.5h6.3c.86-.61 2.35-2.03 2.35-4.5z" opacity=".3"/><path fill="currentColor" d="M7 20h4c0 1.1-.9 2-2 2s-2-.9-2-2zm-2-1h8v-2H5v2zm11.5-9.5c0 3.82-2.66 5.86-3.77 6.5H5.27c-1.11-.64-3.77-2.68-3.77-6.5C1.5 5.36 4.86 2 9 2s7.5 3.36 7.5 7.5zm-2 0C14.5 6.47 12.03 4 9 4S3.5 6.47 3.5 9.5c0 2.47 1.49 3.89 2.35 4.5h6.3c.86-.61 2.35-2.03 2.35-4.5zm6.87-2.13L20 8l1.37.63L22 10l.63-1.37L24 8l-1.37-.63L22 6l-.63 1.37zM19 6l.94-2.06L22 3l-2.06-.94L19 0l-.94 2.06L16 3l2.06.94L19 6z"/></svg></span>Tip</div><div class="admonition-content"><p>Here is some <code>{\`inline code\`}</code></p></div></div>"`)
+  })
 })
